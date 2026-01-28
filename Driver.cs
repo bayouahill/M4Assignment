5bc927f10158b9132610df41d7336dc06c457a0b
bayouahill21 <bayouahill21@gmail.com>
2026-01-28 13:38:43 -0700

    updated class names

---
+++ Driver.cs
@@ -0,0 +1,2 @@
+﻿using M4Assignment;
+
--- Game.cs
+++ Game.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
+/* 
+ * Receive the “board” array from the driver class 
+ * Contain a method that prints the board based on the information passed to it 
+ * Contain a method that receives the game board array as input and returns if there is a winner and who it was
+*/
+
 namespace M4Assignment
 {
+
     internal class Game
     {
+
     }
 }
